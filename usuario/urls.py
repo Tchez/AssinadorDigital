1195cd7243ee2d8fa06fb61dc692ed067682a182
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-08-26 17:20:50 -0300

    feat: :sparkles: adicionando autenticação e inicio da tela gerar chaves

--- usuario/urls.py
+++ usuario/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro/', views.cadastro, name='cadastro'),
-    path('login/', views.login, name='login'),
+    path("", views.index, name="index"),
+    path("auth/cadastro/", views.cadastro, name="cadastro"),
+    path("auth/login/", views.login, name="login"),
+    path("auth/logout/", views.logout, name="logout"),
+    path("gerar_chaves/", views.gerar_chaves, name="gerar-chaves"),    
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,15 +1,60 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import (
+    authenticate,
+    login as login_django,
+    logout as logout_django,
+)
+from django.contrib.auth.decorators import login_required
+
+
+def index(request):
+    return render(request, "index.html")
+
 
 def cadastro(request):
-    if request.method == 'GET':
-        return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, "cadastro.html")
     else:
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
-        
-        return HttpResponse(username)
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        senha = request.POST.get("senha")
+
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            return HttpResponse("Usuário já existe")
+
+        user = User.objects.create_user(username, email, senha)
+        user.save()
+
+        return HttpResponse("Usuário criado com sucesso")
+
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, "login.html")
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        senha = request.POST.get("senha")
+
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            login_django(request, user)
+
+            return render(request, "index.html")
+
+        return HttpResponse("Usuário ou senha inválidos")
+
+
+def logout(request):
+    logout_django(request)
+    return HttpResponseRedirect("/")
+
+
+@login_required(login_url="/auth/login/")
+def gerar_chaves(request):
+    return render(request, "gerar_chaves.html")
